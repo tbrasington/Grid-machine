e054c5c4c043414fada53ea3d0fbc1cab846f5d6
Thomas Brasington <tom@2c2d.co.uk>
2013-05-01 19:10:37 +0100

    basic adding to the canvas

--- js/main.js
+++ js/main.js
@@ -4,26 +4,42 @@ var grid_constructor = function()
 {
 	var that = this;
 	
+	this.grid_canvas = null;
+	
 	this.init = function()
 	{
-		this.events();
+		that.events();
 	}
 	
 	this.events = function()
 	{
-		$('#grid-canvas').on('click', that.build_a_section);
+		that.grid_canvas.on('click', function(e){
+			
+			e.preventDefault();
+			e.stopPropagation();
+			that.build_a_section();
+		});
+		
+		that.grid_canvas.on('click', '.section', function(e){
+			
+			e.preventDefault();
+			e.stopPropagation();
+			that.build_a_block($(this));
+		});
 	}
 	
 	this.build_a_section = function()
 	{
-		var section = $('<div />', { 'class' : 'section group'});
+	
+		var section = $('<div />', { 'class' : 'section group'}).appendTo(that.grid_canvas);
 		
-		return section;
+		//return section;
 	}
 	
-	this.build_a_block = function()
+	this.build_a_block = function(target_section)
 	{
-		
+		console.log('block');
+		var block_element = $('<div />', { 'class' : 'block-element span-2-8'}).appendTo(target_section);
 	}
 }
 
@@ -33,6 +49,7 @@ $(document).ready(function()
 	
 	// build
 	var i_need_a_grid = new grid_constructor();
+		i_need_a_grid.grid_canvas = $('#grid-canvas');
 		i_need_a_grid.init();
 
 });
